c1bc21385ed9e70fea1b1b8ad4fbccd847af51ad
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-03-31 13:31:33 -0600

    Test de Orden de Ejecucion
    - Se crearon 3 scripts para probar el orden de ejecucion.
    - Se edito el codigo de Hola Mundo con mas instrucciones para probar dicho orden.

--- ProyectoInicialEbac/Assets/Scripts/HolaMundo.cs
+++ ProyectoInicialEbac/Assets/Scripts/HolaMundo.cs
@@ -16,6 +16,22 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hola desde update");
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.LogWarning("Hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("El Objeto ha sido habilidato");
+    }
+    private void OnDisable()
+    {
+        Debug.LogError("El objeto ha sido deshabilitado");
     }
 }
