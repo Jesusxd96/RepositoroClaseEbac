8203b42b0fb762ee629dcfa1e4273e0d9b74a725
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-04-16 20:01:45 -0600

    Branch Modulo 6
    - Se creo un branch del modulo 6
    - Se crearon un suelo, un cubo, una esfera y una capsula.
    - Se les agregaron fisicas a dichos objetos.
    - Se creo un script para instanciar cubos y limitarlos para que desaparezcan cuando se reduzcan de tamaño.

---
+++ ProyectoInicialEbac/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject PrefabCubo;
+    public List<GameObject> listaDeCubos;
+    public float factorDeEscalamiento;
+    public int numeroDeCubos=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listaDeCubos = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        numeroDeCubos++;
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCubo);
+        tempGameObject.name = "CuboNumero" + numeroDeCubos;
+        Color c = new Color(Random.value,Random.value,Random.value);
+        tempGameObject.GetComponent<MeshRenderer>().material.color = c;
+        tempGameObject.transform.position = Random.insideUnitSphere;
+        //para limitar los cubos en pantalla
+        listaDeCubos.Add(tempGameObject);
+        List<GameObject> objetosParaEliminar = new List<GameObject>();
+        foreach(GameObject go in listaDeCubos) //El "go" es de gameobject
+        {
+            float scale = go.transform.localScale.x;
+            scale *= factorDeEscalamiento;
+            go.transform.localScale = Vector3.one * scale;
+
+            if (scale <= 0.1) {
+                objetosParaEliminar.Add(go);
+            }
+        }
+
+        foreach(GameObject go in objetosParaEliminar){
+            listaDeCubos.Remove(go);
+            Destroy(go);
+        }
+    }
+}
