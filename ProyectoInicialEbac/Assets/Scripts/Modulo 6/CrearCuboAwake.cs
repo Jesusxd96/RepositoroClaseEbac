170b22390f6fe3e1565d80e08fddb0bce0849123
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-04-17 19:52:50 -0600

    Codigos para generar cubos
    - Se creo el esqueleto de 3 scripts para la creacion de cubos.
    - La creacion sera en el metodo Awake, Update y OnDisable/OnEnable.
    - Estos aun no estan terminados ni implementados

---
+++ ProyectoInicialEbac/Assets/Scripts/Modulo 6/CrearCuboAwake.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrearCubos : MonoBehaviour
+{
+
+    [SerializeField] private GameObject objACrear;
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnEnable()
+    {
+        
+    }
+    void OnDisable()
+    {
+        
+    }
+}
