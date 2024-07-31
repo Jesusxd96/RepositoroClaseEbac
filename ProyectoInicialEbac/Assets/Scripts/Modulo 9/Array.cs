a8cb3d854b421d082f1b85372acb0690cb47897d
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-07-30 21:35:55 -0600

    Practica 9 - Incisos 2 y 3
    - Se implemento la "concatenacion" de las strings de la matriz palabras.
    - Se crearon 1 matriz 2x2 mas aparte 1 vector sencillo y se multiplicaron. El resultado se imprime en la consola.

--- ProyectoInicialEbac/Assets/Scripts/Modulo 9/Array.cs
+++ ProyectoInicialEbac/Assets/Scripts/Modulo 9/Array.cs
@@ -6,23 +6,26 @@ public class Array : MonoBehaviour
 {
 
     /*Arreglos unidimensionales - Llenar 2 con Random.Range y sumar sus resultados en un 3er array.*/
-    int[] arregloA = new int[5], arregloB = new int [5];//Se llenaran aleatoreamente y luego se sumaran.
+    int[] arregloA = new int[5], arregloB = new int[5];//Se llenaran aleatoreamente y luego se sumaran.
     int[] arregloC = new int[5]; //Se usara para guardar el resultado de los 2 anteriores.
-    int min=0, max=10;
+    int min = 0, max = 10;
 
     /*Array de String y llena cada elemento con una palabra de una oracion, luego usando foreach imprimela en un solo string*/
-    string[] palabras;
-    string oracion;//Donde se imprimiran las palabras despues del foreach.
+    string[] palabras = new string[5] { "One ", "Piece ", "me ", "encanta ", ":D" };
+    string oracion = "";//Donde se imprimiran las palabras despues del foreach.
 
     /*Multiplicar arreglo bidimensiona por unidimencional, basicamente matriz por vector*/
-    int[,] matrizA;
-    int[] arregloMultiplicacion;
+    int[,] matrizA = new int[2, 2] { {3,5},{4,6} };
+    int[] arregloD = new int[2] {2,3};
+    int[] resul = new int[2] {0,0};//Lo inicializamos con 0 por cualquier cosa
 
     // Start is called before the first frame update
     void Start()
     {
         GenerateVector();
         SumaMatriz(arregloA,arregloB);
+        GenerateSentence();
+        MultiplicacionMatriz();
     }
 
     // Update is called once per frame
@@ -37,14 +40,14 @@ public class Array : MonoBehaviour
             arregloB[i] = Random.Range(min,max);
         }
         Debug.Log("Arreglo A: \n");
-        for (i = 0; i < 5; i++)//Imprimir la primer matriz
+        for (i = 0; i < arregloA.GetLength(0); i++)//Imprimir la primer matriz
         {
-            Debug.Log("["+ arregloA[i] +"] ");
+            Debug.Log("Posicion " + i + ": ["+ arregloA[i] +"] ");
         }
         Debug.Log("Arreglo B: \n");
-        for (i = 0; i < 5; i++)
+        for (i = 0; i < arregloB.GetLength(0); i++)
         {
-            Debug.Log("[" + arregloB[i] + "] ");
+            Debug.Log("Posicion " + i + ": [" + arregloB[i] + "] ");
         }
     }
     void SumaMatriz(int[] mat1, int[] mat2)
@@ -55,5 +58,22 @@ public class Array : MonoBehaviour
             Debug.Log("[" + arregloC[i] + "] ");
         }
     }
-
+    void GenerateSentence()
+    {
+        foreach(string palabra in palabras)
+        {
+            oracion += palabra;
+        }
+        Debug.Log("La oracion completa es: \n" + oracion);
+    }
+    void MultiplicacionMatriz(){
+        int i = 0,j=0;
+        Debug.Log("El resultado de la multiplicion es: ");
+        for (i = 0; i < 2;i++){
+            for (j = 0; j < 2; j++){
+                resul[i] += matrizA[i,j] * arregloD[i];
+            }
+            Debug.Log("Posicion " + i + ": " +resul[i]);
+        }
+    }
 }
