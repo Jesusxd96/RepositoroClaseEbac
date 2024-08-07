f01d18bb448cbe24ad9464ca4f8e9af80250ecb9
Jesusxd96 <33816389+Jesusxd96@users.noreply.github.com>
2024-08-07 17:01:14 -0600

    Seguimiento video 3
    - Se creo la carpeta referente al modulo 10.
    - Se crearon los script "Clases" y "ClaseNormal" conforme a lo visto en el video.

---
+++ ProyectoInicialEbac/Assets/Scripts/Modulo 10/Practicas/ClaseNormal.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClaseNormal
+{
+    int numero1;
+    int numero2;
+
+    public ClaseNormal(int param1, int param2)//Constructor
+    {
+        numero1 = param1;
+        numero2 = param2;
+    }
+
+    public int SumarEnteros(int a, int b)
+    {
+        return a + b;
+    }
+
+}
